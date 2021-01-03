f0dc64ec7726d84f3246f3e22cdbd3bfae822aa8
ggroce <ggrocedev@gmail.com>
2021-01-02 23:58:59 -0600

    adding page route

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Navigation.css';
 
-const Navigation = () => {
+const Navigation = ({ onRouteChange }) => {
     return(
         <nav>
-            <p className='f3 link dim white-80 underline pa3 pointer'>Sign Out</p>
+            <p onClick={() => onRouteChange('signin')} className='f3 link dim white-80 underline pa3 pointer'>Sign Out</p>
         </nav>
     );
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-const SignIn = ({ onRouteChange }) => {
+const Register = ({ onRouteChange }) => {
     return(
         <article className="br2 ba b--white-10 mv4 w-100 w-50-m w-25-l mw5 center">
             <main className="pa4 white-80">
                 <form className="measure">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+                        <legend className="f4 fw6 ph0 mh0">Register</legend>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f6" for="name">Name</label>
+                            <input className="white-80 pa2 input-reset ba bg-transparent hover-bg-black w-100" 
+                            type="text" name="name"  id="name" />
+                        </div>
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" for="email-address">Email</label>
                             <input className="white-80 pa2 input-reset ba bg-transparent hover-bg-black w-100" 
@@ -20,14 +25,11 @@ const SignIn = ({ onRouteChange }) => {
                     </fieldset>
                     <div className="">
                         <input onClick={() => onRouteChange('home')} className="white-80 b ph3 pv2 input-reset ba b--white bg-transparent grow pointer f6 dib" 
-                        type="submit" value="Sign in" />
-                    </div>
-                    <div className="lh-copy mt3">
-                        <p onClick={() => onRouteChange('register')} className="f6 link dim white db pointer">Register</p>
+                        type="submit" value="Register" />
                     </div>
                 </form>
             </main>
         </article>
     );
 }
-export default SignIn;
+export default Register;
