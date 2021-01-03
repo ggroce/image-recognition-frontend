b4169940bd493875898ccfab9453b16daa587412
Garrett Groce <ggroce@gmail.com>
2021-01-03 16:25:35 -0600

    cleaning up routes

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,21 +4,21 @@ const Register = ({ onRouteChange }) => {
     return(
         <article className="br2 ba b--white-10 mv4 w-100 w-50-m w-25-l mw5 center">
             <main className="pa4 white-80">
-                <form className="measure">
+                <div className="measure">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                         <legend className="f4 fw6 ph0 mh0">Register</legend>
                         <div className="mt3">
-                            <label className="db fw6 lh-copy f6" for="name">Name</label>
+                            <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
                             <input className="white-80 pa2 input-reset ba bg-transparent hover-bg-black w-100" 
                             type="text" name="name"  id="name" />
                         </div>
                         <div className="mt3">
-                            <label className="db fw6 lh-copy f6" for="email-address">Email</label>
+                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                             <input className="white-80 pa2 input-reset ba bg-transparent hover-bg-black w-100" 
                             type="email" name="email-address"  id="email-address" />
                         </div>
                         <div className="mv3">
-                            <label className="db fw6 lh-copy f6" for="password">Password</label>
+                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                             <input className="white-80 b pa2 input-reset ba bg-transparent hover-bg-black w-100" 
                             type="password" name="password"  id="password" />
                         </div>
@@ -27,7 +27,7 @@ const Register = ({ onRouteChange }) => {
                         <input onClick={() => onRouteChange('home')} className="white-80 b ph3 pv2 input-reset ba b--white bg-transparent grow pointer f6 dib" 
                         type="submit" value="Register" />
                     </div>
-                </form>
+                </div>
             </main>
         </article>
     );
